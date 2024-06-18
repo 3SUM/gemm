a30515b45ea583f4b75d91288a74f2847b8bae89
Luis Maya Aranda <mayafluis@gmail>
2024-06-17 21:20:04 -0700

    removed print

--- src/gemm.py
+++ src/gemm.py
@@ -1,14 +1,12 @@
 import numpy as np
 
-m = 1024
-n = 512
-k = 2048
+M = 1024
+N = 512
+K = 2048
 
-A = np.random.randn(m, k).astype(np.float32)
-B = np.random.randn(k, n).astype(np.float32)
+A = np.random.randn(M, K).astype(np.float32)
+B = np.random.randn(K, N).astype(np.float32)
 C = np.dot(A, B)
-
-print(C)
 
 with open("data.bin", "wb") as f:
     f.write(A.data)
